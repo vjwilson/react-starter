2dc830879697cd44c4154132bbd6054a11f29b7f
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2016-09-02 23:04:04 -0400

    Add webpack to build process

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './src/app',
+  output: {
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react']
+        }
+      },
+    ]
+  }
+};
